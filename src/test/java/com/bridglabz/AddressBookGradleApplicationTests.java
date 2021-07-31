ec31b4c42858a624899e64c1538bc986b0c061bd
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-31 19:59:04 +0530

    Added:program to create adress book and add details of each contact into the adress book

---
+++ src/test/java/com/bridglabz/AddressBookGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridglabz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
